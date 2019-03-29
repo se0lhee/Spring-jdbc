1592a286d80397059e4f1a4c414aab76947b194f
se0lhee <gs05074@naver.com>
2019-03-29 14:55:11 +0900

    add src

---
+++ src/main/java/com/seolhee/book/chap03/MemberMain.java
@@ -0,0 +1,25 @@
+package com.seolhee.book.chap03;
+
+/**
+ * MemberRegisterService를 테스트한다.<br>
+ * new를 사용해서 직접 인스턴스를 생성한다.
+ * 
+ * @author Jacob
+ */
+public class MemberMain {
+
+	public static void main(String[] args) {
+		MemberDao memberDao = new MemberDao();
+		MemberRegisterService regService = new MemberRegisterService();
+		regService.setMemberDao(memberDao);
+
+		// registerRequest 초기화
+		RegisterRequest req = new RegisterRequest();
+		req.setEmail("gs05074@naver.com");
+		req.setPassword("027019");
+		req.setName("seolhee");
+
+		// 회원 등록
+		regService.regist(req);
+	}
+}
