0b26168baa6a75ef59a345606f4bb02c44b06d1a
se0lhee <gs05074@naver.com>
2019-04-05 14:52:57 +0900

    add src

---
+++ src/main/java/com/seolhee/article/ArticleService.java
@@ -0,0 +1,73 @@
+package com.seolhee.article;
+
+import java.util.List;
+
+/**
+ * articleDao를 사용해서 글 목록, 조회, 추가, 수정, 삭제를 한다.
+ * 
+ * @author Jacob
+ */
+public class ArticleService {
+
+	ArticleDao articleDao;
+
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	/**
+	 * 글 목록
+	 */
+	public void listArticles() {
+		List<Article> articles = articleDao.listArticles();
+		System.out.println(articles);
+	}
+
+	/**
+	 * 글 조회
+	 */
+	public void getArticle() {
+		Article article = articleDao.getArticle("3");
+		System.out.println(article);
+	}
+
+	/**
+	 * 글 등록
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		article.setTitle("‘강원 산불’ 불똥 맞은 나경원···“심각성 몰랐다” 해명도 논란");
+		article.setContent(
+				"4일 국회 운영위서 정의용 국가안보실장 이석 방해 논란 / 나경원 \"회의 집중하느라 산불 알지 못했다\" 해명 / 일각서 \"해명이 화 키우고 있다\" 지적도 / \"질문이 중요하냐, 생명이 중요하냐\" 與, 한국당 맹비난");
+		article.setUserId("1");
+		article.setName("배설희");
+		if (articleDao.addArticle(article) > 0)
+			System.out.println("글을 추가했습니다.");
+		else
+			System.out.println("글을 추가하지 못했습니다.");
+	}
+
+	/**
+	 * 글 수정
+	 */
+	public void updateArticle() {
+		Article article = new Article();
+		article.setArticleId("7");
+		article.setTitle("This is modified title.");
+		article.setContent("This is modified content");
+		if (articleDao.updateArticle(article) > 0)
+			System.out.println("글을 수정했습니다.");
+		else
+			System.out.println("글을 수정하지 못했습니다.");
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	public void deleteArticle() {
+		if (articleDao.deleteArticle("8") > 0)
+			System.out.println("글을 삭제했습니다.");
+		else
+			System.out.println("글을 삭제하지 못했습니다.");
+	}
+}
